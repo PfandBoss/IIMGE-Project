7dc2209d855c2e46be7826005bc1e8b7215a7d1f
Jonas <68470553+Herr-Kakashi@users.noreply.github.com>
2022-01-06 15:19:26 +0100

    Movement
    Character Movement added (No character signals yet)

---
+++ Imge Project/Assets/Features/FirstPersonController/Controller/FirstPersonController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+
+/// <summary>
+///     Controller that handles the character controls and camera controls of the first person player.
+/// </summary>
+[RequireComponent(typeof(CharacterController))]
+public class FirstPersonController : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private FirstPersonControllerInput firstPersonControllerInput;
+    private CharacterController _characterController;
+    private Camera _camera;
+
+    [SerializeField]private float walkingSpeed;
+    private float velocity;
+    [SerializeField] private float sprintingSpeed;
+    [SerializeField] private float maxViewAngle;
+    [SerializeField] private float minViewAngle;
+    [SerializeField] private float jumpSpeed;
+    
+    private float stickToGroundForceMagnitude = 5.0f;
+
+
+    private void Awake() {
+        _characterController = GetComponent<CharacterController>();
+        _camera = GetComponentInChildren<Camera>();
+        velocity = walkingSpeed;
+    }
+
+    private void Start()
+    {
+        _characterController.Move(-stickToGroundForceMagnitude * transform.up);
+        
+        var jumpLatch = LatchObservables.Latch(this.UpdateAsObservable(), firstPersonControllerInput.Jump, false);
+        
+        firstPersonControllerInput.Move.Zip(jumpLatch,(mo,ju) => new MoveInputData(mo,ju) ).Where(inputData => inputData.move  != Vector2.zero|| inputData.jump ).Subscribe(input =>
+        {
+            var wasGrounded = _characterController.isGrounded;
+            var verticalVelocity = 0f;
+            if (input.jump && wasGrounded)
+            {
+        // We're on the ground and want to jump.
+                verticalVelocity = jumpSpeed;
+                
+            }
+            else if (!wasGrounded)
+            {
+        // We're in the air: apply gravity.
+                verticalVelocity = _characterController.velocity.y +
+                                   (Physics.gravity.y * Time.deltaTime * 3.0f);
+            }
+            else
+            {
+        // We're on the ground: push us down a little.
+        // (Required for character.isGrounded to work.)
+                verticalVelocity = -Mathf.Abs(stickToGroundForceMagnitude);
+            }
+
+            
+
+            var horVelocity = input.move * walkingSpeed;
+
+            var charVelocity = transform.TransformVector(new Vector3(horVelocity.x, verticalVelocity, horVelocity.y));
+ 
+            var distance = charVelocity * Time.deltaTime;
+
+            _characterController.Move(distance);
+        }).AddTo(this);
+
+        firstPersonControllerInput.Look.Where(vec => vec != Vector2.zero).Subscribe(input =>
+        {
+            var horLook = input.x * Vector3.up * Time.deltaTime;
+            
+            transform.localRotation *= Quaternion.Euler(horLook);
+
+
+            var vertLook = input.y * Vector3.left * Time.deltaTime ;
+
+
+            var newQ = _camera.transform.localRotation * Quaternion.Euler(vertLook);
+
+
+            _camera.transform.localRotation = RotationTools.ClampRotationAroundXAxis(newQ, -maxViewAngle, -minViewAngle);
+        }).AddTo(this);
+
+
+        firstPersonControllerInput.Run.Subscribe(input =>
+        {
+            if (input)
+            {
+                walkingSpeed = sprintingSpeed;
+            }
+            else
+            {
+                walkingSpeed = velocity;
+            }
+
+        });
+
+    }
+    
+    
+
+    public struct MoveInputData
+    {
+        public readonly Vector2 move;
+        public readonly bool jump;
+        public MoveInputData(Vector2 move, bool jump) {
+            this.move = move;
+            this.jump = jump;
+        }
+
+    }
+
+}
---
+++ Imge Project/Assets/Features/FirstPersonController/Input/PlayerInput/InputActionBasedFirstPersonControllerInput.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class InputActionBasedFirstPersonControllerInput : FirstPersonControllerInput
+{
+    private FirstPersonInputAction _firstPersonInputAction;
+
+
+    private IObservable<Vector2> _move;
+    private IObservable<Vector2> _look;
+    private ReadOnlyReactiveProperty<bool> _sprint;
+    private Subject<Unit> _jump;
+
+
+
+    private const float LookSmoothingFactor = 50f;
+
+    public override IObservable<Vector2> Move
+    {
+        get { return _move; }
+    }
+    public override IObservable<Unit> Jump
+    {
+        get { return _jump; }
+    }
+    public override ReadOnlyReactiveProperty<bool> Run
+    {
+        get { return _sprint; }
+    }
+    public override IObservable<Vector2> Look
+    {
+        get { return _look; }
+    }
+
+    private void Awake()
+    {
+        _firstPersonInputAction = new FirstPersonInputAction();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        
+        _move = this.UpdateAsObservable()
+            .Select(_ => _firstPersonInputAction.Char.Move.ReadValue<Vector2>());
+
+        var smoothLookValue = new Vector2(0, 0);
+        _look = this.UpdateAsObservable()
+            .Select(_ =>
+            {
+                var rawLookValue = _firstPersonInputAction.Char.Look.ReadValue<Vector2>();
+
+                smoothLookValue = new Vector2(
+                    Mathf.Lerp(smoothLookValue.x, rawLookValue.x, LookSmoothingFactor * Time.deltaTime),
+                    Mathf.Lerp(smoothLookValue.y, rawLookValue.y, LookSmoothingFactor * Time.deltaTime)
+                );
+
+                return smoothLookValue;
+            });
+
+
+        _sprint = this.UpdateAsObservable().Select(_=> _firstPersonInputAction.Char.Sprint.ReadValueAsObject() != null).ToReadOnlyReactiveProperty();
+
+
+        _jump = new Subject<Unit>().AddTo(this);
+        _firstPersonInputAction.Char.Jump.performed += _ => _jump.OnNext(Unit.Default);
+
+    }
+
+    private void OnEnable()
+    {
+        _firstPersonInputAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _firstPersonInputAction.Disable();
+    }
+    
+    
+}
