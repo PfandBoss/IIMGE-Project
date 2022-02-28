0ee67fab4fb20ce8d36fa9b90ff75b51f76d582a
INoro <okay.uenal@tum.de>
2022-02-28 22:41:11 +0100

    Win Screen

--- Assets/Features/EnemyAI/Scripts/EnemySpawner.cs
+++ Assets/Features/EnemyAI/Scripts/EnemySpawner.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemySpawner : MonoBehaviour
 {
@@ -51,7 +52,9 @@ public class EnemySpawner : MonoBehaviour
             return;
         }
         
-        //Win Screen here
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+        SceneManager.LoadScene("Win Screen");
     }
 
     public void DisableHolo()
